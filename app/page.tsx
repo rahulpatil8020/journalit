0dc432db22cceb42553face5a08d814f9b906456
Rahul Patil <rahuldpatil20@gmail.com>
2024-06-01 16:50:41 -0700

    changed some pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-       <div>Hello</div>
-    </main>
+    <>
+      <span className="font-bold text-4xl">Home</span>
+      <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+    </>
   );
 }
